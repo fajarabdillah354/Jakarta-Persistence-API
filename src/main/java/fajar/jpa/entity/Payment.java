13823ebf98f0d6f4a775b580a55d84ab473156f7
unknown <fajardillah25@gmail.com>
2024-11-25 08:53:00 +0700

    Upload

---
+++ src/main/java/fajar/jpa/entity/Payment.java
@@ -0,0 +1,21 @@
+package fajar.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "payments")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Data
+public class Payment {
+
+    @Id
+    private String id;
+
+
+    private Long amount;
+
+
+
+
+}
---
+++ src/main/java/fajar/jpa/entity/PaymentCreditCard.java
@@ -0,0 +1,18 @@
+package fajar.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "payments_credit_card")
+@Data
+public class PaymentCreditCard extends Payment{
+
+    @Column(name = "masked_card")
+    private String maskedCard;
+
+    private String bank;
+
+}
